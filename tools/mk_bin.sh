bb9e675cf1d37a6bf392fa0011bbd6b22bf5d8e4
wemos <support@wemos.cc>
2026-04-23 15:11:03 +0800

    fix mk_bin.sh

--- tools/mk_bin.sh
+++ tools/mk_bin.sh
@@ -2,5 +2,6 @@
 # git describe | xargs git tag
 # git describe | xargs proxychains -q git push origin 
 git push
+git push origin --tags
 git describe | xargs git tag
 git describe | xargs git push origin 
